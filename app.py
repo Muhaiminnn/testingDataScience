3fc3e608604be11581872faa0779a4e90d0ec1b4
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-12 16:19:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ def main():
     nim = st.text_input("Masukkan NIM:")
     if nim == "200205501009":
         nama_pengguna = "Muhaimin"
+        st.write(f'Selamat Seminar Proposal, {nama_pengguna}!')
+        gambar_lokal = 'bah sod.jpg'
+        st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
     elif nim == "":
         st.write(f'Masukin NIM dulu lah kocak!')
     else:
         st.write(f'Lah kamu ga diajak...')
-    st.write(f'Selamat Seminar Proposal, {nama_pengguna}!')
 
 if __name__ == '__main__':
     main()
