8b7da87b99ce23f00b51e502617cb9820ef7e186
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-12 16:42:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,22 @@ def main():
         st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
     elif nim == "200205502002":
         nama_pengguna = "Yusraul Fitrah"
-        st.write(f'Sudah mki kita {nama_pengguna}, tapi semangatki jalani proses dan semoga ketika baca ini masih langgeng sama si dia')
+        st.write(f'Sudah mki kita' {nama_pengguna}, tapi semangatki jalani proses. Dan tunggui teman satu dosen PA ta kodong.')
+        #gambar_lokal = 'bah sod.jpg'
+        #st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
+    elif nim == "200205501004":
+        nama_pengguna = "Hasril"
+        st.write(f'Baru-baru sudah {nama_pengguna} sempro nah, nah kita mi itu paling lancar bimbingan.')
+        #gambar_lokal = 'bah sod.jpg'
+        #st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
+    elif nim == "200205502015":
+        nama_pengguna = "Muhammad Ikram"
+        st.write(f'Ededeh apaji kau {nama_pengguna}, kah lamami kau sudah.')
+        #gambar_lokal = 'bah sod.jpg'
+        #st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
+    elif nim == "200205500002":
+        nama_pengguna = "Yaya Ferida"
+        st.write(f'wiss nih {nama_pengguna} nih boss *Ferdian, le'ba' tongmi tawwa')
         #gambar_lokal = 'bah sod.jpg'
         #st.image(gambar_lokal, caption='Korban Tumbuk Dada', use_column_width=True)
     elif nim == "":
