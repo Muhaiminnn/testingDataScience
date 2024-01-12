7a2e4da6477b9e19ffdac1cf6fc4b03327181874
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-12 16:02:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ def main():
     st.title("Aplikasi Streamlit dengan Ngrok")
 
     # Tambahkan elemen-elemen Streamlit
-    nim = st.text_input("Masukkan NIM:", "Default")
+    nim = st.text_input("Masukkan NIM:")
     if nim == "200205501009":
         nama_pengguna = "Muhaimin"
+    else:
+        st.write(f'Lah kamu ga diajak...')
     st.write(f'Selamat Seminar Proposal, {nama_pengguna}!')
 
 if __name__ == '__main__':
