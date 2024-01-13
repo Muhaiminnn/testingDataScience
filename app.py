0b516def136a3699a52b19d23f1d263fde00476d
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-13 19:51:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Jalankan perintah instalasi dependensi
-subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+#subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
 
 import streamlit as st
 import pandas as pd
@@ -11,12 +11,25 @@ import seaborn as sns
 data = pd.read_csv('dailybrentoil.csv')
 kolom_yang_dihapus = ['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9']
 data = data.drop(kolom_yang_dihapus, axis=1)
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
+
+def polaTren():
+    plt.figure(figsize=(12, 6))
+    plt.plot(data['Open'], label='Open Price')
+    plt.title('Harga Open over Time')
+    plt.xlabel('Tanggal')
+    plt.ylabel('Harga Open')
+    plt.legend()
+    plt.show()
+    return plt
 
 def main():
     st.title("Mini Project Prediksi Harga Minyak Dunia By Muhaimin")
     #st.table(data)
     st.write("Deskripsi Data")
     st.write(data.describe())
+    st.pyplot(polaTren())
 
 if __name__ == '__main__':
     main()
