4260255bb8fb818aad467a0ac14c39011c31c9bb
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-13 19:15:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ import subprocess
 subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
 
 import streamlit as st
-import gspread
-
-spreadsheet_key = '1T90XmDuGPtkPtQwC2b1Yt0e5cJiPNfgPcSGvMfLEtDk'
-worksheet_name = 'dailybrentoil'
-gc = gspread.service_account()
-
-sh = gc.open_by_key(spreadsheet_key)
-worksheet = spreadsheet.worksheet(worksheet_name)
-# Ambil data
-data = worksheet.get_all_records()
+import pandas as pd
+data = pd.read_csv('dailybrentoil.csv')
 
 def main():
     st.title("Prediksi Harga Minyak Dunia By Muhaimin\n Mini Project Data Scientist Kalla")
