5c0343e3ced4b46b93bd6a96149d2200d7d95f12
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-13 19:43:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,10 @@ data = pd.read_csv('dailybrentoil.csv')
 kolom_yang_dihapus = ['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9']
 data = data.drop(kolom_yang_dihapus, axis=1)
 
-def create_plot():
-    numerical_col = [col for col in data.columns if data[col].dtypes == 'float64']
-    plt.subplots(figsize=(10,7))
-    sns.boxplot(data=data[numerical_col]).set_title("Gold Price")
-    plt.show()
-    return plt
-
 def main():
     st.title("Prediksi Harga Minyak Dunia By Muhaimin\n Mini Project Data Scientist Kalla")
     #st.table(data)
-    st.pyplot(create_plot())
+    st.write(data.describe())
 
 if __name__ == '__main__':
     main()
