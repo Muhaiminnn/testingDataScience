4a858efa51f364b496ca7405724dda0ef513143f
Muhaiminnn <156325118+Muhaiminnn@users.noreply.github.com>
2024-01-14 16:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import seaborn as sns
 import streamlit as st
 
 # -- Import File --
-file = "/testingDataScience/brentcrudeoil.xlsx"
+file = "brentcrudeoil.xlsx"
 
 df_daily = pd.read_excel(file, sheet_name='dailybrentoil')
 df_daily = df_daily.iloc[:, :-3]
